54abf35dd9b713e3128c097a8912abda93975124
Samee Vohra <samee.vohra@gmail.com>
2023-03-15 14:42:57 -0700

    eslint, prettier, and other initial setup

---
+++ app.js
@@ -0,0 +1,42 @@
+const questionDisplay = document.querySelector('#questions');
+const answerDisplay = document.querySelector('#answer');
+
+// const questions = [
+//   {
+//     id: 0,
+//     text: 'Pick a vacation destination:',
+//     answers: [
+//       {
+//         text: 'New York',
+//         image: ,
+//         alt: ,
+//         credit:
+//       }
+//     ]
+//   },
+//   {
+//     id: ,
+//     text: ,
+//     answers: [
+//       {
+//         text: ,
+//         image: ,
+//         alt: ,
+//         credit:
+//       }
+//     ]
+//   },
+//   {
+//     id: ,
+//     text: ,
+//     answers: [
+//       {
+//         text: ,
+//         image: ,
+//         alt: ,
+//         credit:
+//       }
+//     ]
+//   },
+
+// ]
