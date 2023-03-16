5679e0c304f84dcac936503b7ef5a057cc58d132
Samee Vohra <samee.vohra@gmail.com>
2023-03-15 17:27:01 -0700

    rendering questions and answers + styling

--- app.js
+++ app.js
@@ -9,98 +9,98 @@ const questions = [
       {
         text: 'New York',
         image:
-          'https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
-        alt: 'Photo of Times Square with people crossing the street',
-        credit: 'Luca Bravo',
+          'https://images.unsplash.com/photo-1534430480872-3498386e7856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges',
+        alt: 'Photo of the Empire State building during daytime',
+        credit: 'Oliver Niblett',
       },
       {
         text: 'Austin',
         image:
-          'https://images.unsplash.com/photo-1561047725-c5808d089cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
-        alt: 'Photo of an atx sign in Austin',
-        credit: 'Tomek Baginski',
+          'https://images.unsplash.com/photo-1531218150217-54595bc2b934?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges',
+        alt: 'Time-lapse photography car lights on bridge',
+        credit: 'Carlos Alfonso',
       },
       {
-        text: 'San Francisco',
+        text: 'Portland',
         image:
-          'https://images.unsplash.com/photo-1501594907352-04cda38ebc29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
-        alt: 'Photo of the Golden Gate Bridge from an aerial perspective',
-        credit: 'Maarten can den Heuvel',
+          'https://images.unsplash.com/photo-1534430480872-3498386e7856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges',
+        alt: 'High-rise buildings',
+        credit: 'Elena Kuchko',
       },
       {
         text: 'New Orleans',
         image:
-          'https://images.unsplash.com/photo-1568693059993-a239b9cd4957?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80',
-        alt: 'Photo of the French Quarter in New Orleans',
-        credit: 'Rosie Kerr',
+          'https://images.unsplash.com/photo-1549965738-e1aaf1168943?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges',
+        alt: 'Road with people and house',
+        credit: 'João Francisco',
       },
     ],
   },
   {
     id: 1,
-    text: 'Pick a vacation destination:',
+    text: 'Pick some food:',
     answers: [
       {
-        text: 'New York',
+        text: 'Pizza',
         image:
-          'https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
-        alt: 'Photo of Times Square with people crossing the street',
-        credit: 'Luca Bravo',
+          'https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges',
+        alt: 'Pepperoni Pizza',
+        credit: 'Alan Hardman',
       },
       {
-        text: 'Austin',
+        text: 'Sandwich',
         image:
-          'https://images.unsplash.com/photo-1561047725-c5808d089cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
-        alt: 'Photo of an atx sign in Austin',
-        credit: 'Tomek Baginski',
+          'https://images.unsplash.com/photo-1481070414801-51fd732d7184?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges',
+        alt: 'ham sandwich on white surface',
+        credit: 'Eaters Collective',
       },
       {
-        text: 'San Francisco',
+        text: 'Pasta',
         image:
-          'https://images.unsplash.com/photo-1501594907352-04cda38ebc29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
-        alt: 'Photo of the Golden Gate Bridge from an aerial perspective',
-        credit: 'Maarten can den Heuvel',
+          'https://images.unsplash.com/photo-1516100882582-96c3a05fe590?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges',
+        alt: 'Pasta in tomato sauce',
+        credit: 'Mgg Vitchakorn',
       },
       {
-        text: 'New Orleans',
+        text: 'Hamburger',
         image:
-          'https://images.unsplash.com/photo-1568693059993-a239b9cd4957?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80',
-        alt: 'Photo of the French Quarter in New Orleans',
-        credit: 'Rosie Kerr',
+          'https://images.unsplash.com/photo-1551782450-a2132b4ba21d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges',
+        alt: 'hamburger',
+        credit: 'sk',
       },
     ],
   },
   {
     id: 2,
-    text: 'Pick a vacation destination:',
+    text: 'Pick a home:',
     answers: [
       {
-        text: 'New York',
+        text: 'Traditional',
         image:
-          'https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
-        alt: 'Photo of Times Square with people crossing the street',
-        credit: 'Luca Bravo',
+          'https://images.unsplash.com/photo-1555040479-c949debe66c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges',
+        alt: 'focus photography of building windows',
+        credit: 'Burgess Milner',
       },
       {
-        text: 'Austin',
+        text: 'Modern',
         image:
-          'https://images.unsplash.com/photo-1561047725-c5808d089cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
-        alt: 'Photo of an atx sign in Austin',
-        credit: 'Tomek Baginski',
+          'https://images.unsplash.com/photo-1460317442991-0ec209397118?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges',
+        alt: 'low angle view of building',
+        credit: 'Brandon Giggs',
       },
       {
-        text: 'San Francisco',
+        text: 'House',
         image:
-          'https://images.unsplash.com/photo-1501594907352-04cda38ebc29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
-        alt: 'Photo of the Golden Gate Bridge from an aerial perspective',
-        credit: 'Maarten can den Heuvel',
+          'https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges',
+        alt: 'trees beside white house',
+        credit: 'Phil Hearing',
       },
       {
-        text: 'New Orleans',
+        text: 'Mountains',
         image:
-          'https://images.unsplash.com/photo-1568693059993-a239b9cd4957?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80',
-        alt: 'Photo of the French Quarter in New Orleans',
-        credit: 'Rosie Kerr',
+          'https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges',
+        alt: 'brown wooden cabin infront of forest',
+        credit: 'eulauretta',
       },
     ],
   },
@@ -111,12 +111,52 @@ const populateQuestions = () => {
     const titleBlock = document.createElement('div');
     titleBlock.id = question.id;
     titleBlock.classList.add('title-block');
+
     const titleHeading = document.createElement('h2');
     titleHeading.textContent = question.text;
     titleBlock.append(titleHeading);
 
     questionDisplay.append(titleBlock);
+
+    const answersBlock = document.createElement('div');
+    answersBlock.id = `${question.id}-questions`;
+    answersBlock.classList.add('answer-options');
+
+    questionDisplay.append(answersBlock);
+
+    question.answers.forEach((answer) => {
+      const answerBlock = document.createElement('div');
+      answerBlock.classList.add('answer-block');
+      answerBlock.addEventListener('click', handleClick);
+
+      const answerImg = document.createElement('img');
+      answerImg.setAttribute('src', answer.image);
+      answerImg.setAttribute('alt', answer.alt);
+
+      const answerTitle = document.createElement('h3');
+      answerTitle.textContent = answer.text;
+
+      
+      const answerInfo = document.createElement('p');
+      const imageLink = document.createElement('a');
+      imageLink.setAttribute('href', answer.image);
+      imageLink.textContent = answer.credit;
+      
+      const sourceLink = document.createElement('a');
+      sourceLink.textContent = 'Unsplash';
+      sourceLink.setAttribute('src', 'https://www.unsplash.com');
+      
+      answerInfo.append(imageLink, ' to ', sourceLink);
+
+      answerBlock.append(answerImg, answerTitle, answerInfo);
+      answersBlock.append(answerBlock);
+    });
+    questionDisplay.append(answersBlock);
   });
+};
+
+const handleClick = () => {
+  console.log('Clicked! - to be updated');
 };
 
 populateQuestions();
